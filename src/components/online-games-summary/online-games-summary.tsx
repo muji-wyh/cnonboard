38d9672dcf28c538d155fb6ea794512af8a9bec3
Yoki <yaohuiwang@microsoft.com>
2024-10-14 19:57:15 +0800

    add gameDataContext

--- src/components/online-games-summary/online-games-summary.tsx
+++ src/components/online-games-summary/online-games-summary.tsx
@@ -1,21 +1,35 @@
 import { Divider } from "antd";
 import "./online-games-summary.css";
+import { useContext } from "react";
+import { StoreContext } from "../../configs/store-context.ts";
 
 export const OnlineGamesSummary = () => {
+  const storeContext = useContext(StoreContext);
+
+  const { allMsnGames, allMsnGamesByVendor } = storeContext;
+
+  // todo-Yoki
+  console.info(">>> allMsnGames", allMsnGames, allMsnGamesByVendor);
+
   return (
     <div className="">
       <p className="">现有线上游戏的统计</p>
 
-      <h4 className="">总数量</h4>
+      <h4 className="">总数量: {allMsnGames.length}</h4>
+
       <Divider />
 
-      <h4 className="by-pc-web">按分类</h4>
-      <div className="by-pc-web">
-        <span className="">PC: 0</span>
-        <span className="">WEB: 0</span>
-        <span className="">PC + WEB: 0</span>
-        <span className="">其他: 0</span>
-      </div>
+      <h4 className="">按分类</h4>
+      <ul className="genre-list">
+        {Object.keys(allMsnGamesByVendor).map((vendor) => (
+          <li key={vendor} className="genre-item">
+            <span className="label">{vendor}</span>
+            <span className="content">
+              {allMsnGamesByVendor[vendor].length}
+            </span>
+          </li>
+        ))}
+      </ul>
       <Divider />
     </div>
   );
