e6dda082fcbe622dde5e71c165957d8284e9fbd0
Yoki Wang <yaohuiwang@microsoft.com>
2024-12-13 22:43:00 +0800

    7k7k add pending

--- src/configs/pending_7k7k.ts
+++ src/configs/pending_7k7k.ts
@@ -5,4 +5,19 @@ hmxcj872
 hmsdld856
 htxc853
 hklnlp865
-` + ``;
+` + // 12-13
+  `hqmnhbx656
+httdmg860
+xhjkg99
+xmt98
+xbhr97
+xswmdw94
+hxjqx868
+hwmzb881
+hmhby877
+hynyh878
+hljxs884
+hyxlc885
+hjzsj882
+hzg879
+hshzj876`;
